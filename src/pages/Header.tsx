1afd8f7a9957b5e0b8ed8aab5370f9c9a16ec76b
tarikraposo <tarikraposo@gmail.com>
2024-08-27 01:52:28 -0300

    adding images to assets folder

---
+++ src/pages/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex p-5 bg-[rgb(0,0,0,0.85)] text-white text-xl justify-end gap-6">
+      <p>Comunidade</p>
+      <p>Registrar</p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import background from "../assets/images/bg_intro.jpg"
+import "../style.css"
+const Home = () => {
+  return (
+    <div className="bg-home" >
+      <img src={background} alt="" />
+      
+    </div>
+  );
+};
+
+export default Home;
